362b3abcf0556da50d54a4e831a2a84bf3d542e5
hamsham <mileslacey@gmail.com>
2020-01-10 21:51:13 -0800

    Added more visibility to the chunking test.

--- soft_render/tests/sr_vertex_chunking_test.cpp
+++ soft_render/tests/sr_vertex_chunking_test.cpp
@@ -1,4 +1,5 @@
 
+#include <cassert>
 #include <iostream>
 
 #include "soft_render/SR_ShaderProcessor.hpp"
@@ -6,29 +7,38 @@
 int main()
 {
     constexpr size_t vertsPerPrim = 3;
-    size_t totalVerts = 354u;
-    size_t numThreads = 14u;
-    size_t start;
-    size_t end;
+    constexpr size_t totalVerts = 66u;
+    constexpr size_t numThreads = 5u;
+    constexpr bool lastThreadHasLess = true;
+    size_t start = 0;
+    size_t end = 0;
+    size_t stdChunkSize = 0;
+    size_t endChunkSize = 0;
+
+    static_assert(totalVerts % vertsPerPrim == 0, "Vertices per primitive is not a multiple of the total vertex count.");
 
     for (size_t i = 0; i < numThreads; ++i)
     {
-        std::cout << i << ": " << std::endl;
+        sr_calc_indexed_parition<vertsPerPrim, lastThreadHasLess>(totalVerts, numThreads, i, start, end);
+        endChunkSize = (end == start) ? endChunkSize : end-start;
 
-        sr_calc_indexed_parition<vertsPerPrim, false>(totalVerts, numThreads, i, start, end);
-        
+        std::cout << i << ": " << start << '-' << end << std::endl;
+
         for (size_t j = 0; start < end; ++start, ++j)
         {
             std::cout << "\t" << j << ": " << start << std::endl;
         }
     }
 
-    sr_calc_indexed_parition<vertsPerPrim, false>(totalVerts, numThreads, 0, start, end);
-    size_t stdChunkSize = end-start;
+    sr_calc_indexed_parition<vertsPerPrim, lastThreadHasLess>(totalVerts, numThreads, 0, start, end);
+    stdChunkSize = end;
 
     std::cout << "Total Threads:  " << numThreads   << std::endl;
     std::cout << "Array Size:     " << totalVerts   << std::endl;
     std::cout << "Std Chunk Size: " << stdChunkSize << std::endl;
+    std::cout << "Std Prim Count: " << stdChunkSize/vertsPerPrim << std::endl;
+    std::cout << "End Chunk Size: " << endChunkSize << std::endl;
+    std::cout << "End Prim Count: " << endChunkSize/vertsPerPrim << std::endl;
 
     return 0;
 }
