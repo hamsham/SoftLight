24e165248f492356532b62a87888e00e591237cb
hammy <mileslacey@gmail.com>
2020-05-31 13:17:50 -0700

    Fixed duplicate Window Event enums. Minor code cleanup for readability.

--- soft_render/include/soft_render/SR_WindowEvent.hpp
+++ soft_render/include/soft_render/SR_WindowEvent.hpp
@@ -18,34 +18,27 @@ enum SR_KeySymbol : uint32_t; // KeySym*.hpp
 -----------------------------------------------------------------------------*/
 enum SR_WinEventType : uint32_t
 {
-    WIN_EVENT_NONE = 0x00000001,
+    WIN_EVENT_NONE              = 0x00000000, // No usable event data
+    WIN_EVENT_UNKNOWN           = 0xFE000000,
+    WIN_EVENT_INVALID           = 0xFF000000,
 
-    // SR_MouseButtonEvent
-    WIN_EVENT_MOUSE_BUTTON_DOWN = 0x00000001,
-    WIN_EVENT_MOUSE_BUTTON_UP = 0x00000002,
+    WIN_EVENT_MOUSE_BUTTON_DOWN = 0x00000001, // SR_MouseButtonEvent
+    WIN_EVENT_MOUSE_BUTTON_UP   = 0x00000002,
 
-    // SR_WheelEvent
-    WIN_EVENT_MOUSE_WHEEL_MOVED = 0x00000004,
+    WIN_EVENT_MOUSE_WHEEL_MOVED = 0x00000004, // SR_WheelEvent
 
-    // SR_MousePosEvent
-    WIN_EVENT_MOUSE_MOVED = 0x00000008,
-    WIN_EVENT_MOUSE_ENTER = 0x00000010,
-    WIN_EVENT_MOUSE_LEAVE = 0x00000020,
+    WIN_EVENT_MOUSE_MOVED       = 0x00000008, // SR_MousePosEvent
+    WIN_EVENT_MOUSE_ENTER       = 0x00000010,
+    WIN_EVENT_MOUSE_LEAVE       = 0x00000020,
 
-    // SR_KeyEvent
-    WIN_EVENT_KEY_DOWN = 0x00000040,
-    WIN_EVENT_KEY_UP = 0x00000080,
+    WIN_EVENT_KEY_DOWN          = 0x00000040, // SR_KeyEvent
+    WIN_EVENT_KEY_UP            = 0x00000080,
 
-    // SR_WinUpdateEvent
-    WIN_EVENT_CLOSING = 0x00000100,
-    WIN_EVENT_HIDDEN = 0x00000200,
-    WIN_EVENT_EXPOSED = 0x00000400,
-    WIN_EVENT_RESIZED = 0x00000800,
-    WIN_EVENT_MOVED = 0x00001000,
-
-    // No event data
-    WIN_EVENT_UNKNOWN = 0xFE000000,
-    WIN_EVENT_INVALID = 0xFF000000
+    WIN_EVENT_CLOSING           = 0x00000100, // SR_WinUpdateEvent
+    WIN_EVENT_HIDDEN            = 0x00000200,
+    WIN_EVENT_EXPOSED           = 0x00000400,
+    WIN_EVENT_RESIZED           = 0x00000800,
+    WIN_EVENT_MOVED             = 0x00001000,
 };
 
 
