01efe52b29eea6e81ded773be78daa32068ab442
hammy <mileslacey@gmail.com>
2020-08-15 22:06:44 -0700

    Minor cleanup of trilinear filtering.

--- soft_render/include/soft_render/SR_Sampler.hpp
+++ soft_render/include/soft_render/SR_Sampler.hpp
@@ -326,7 +326,6 @@ inline LS_INLINE color_type sr_sample_trilinear(const SR_Texture& tex, float x, 
         return color_type{0};
     }
 
-    typedef typename SR_Texture::fixed_type fixed_type;
     constexpr WrapMode wrapMode;
 
     /*
@@ -342,35 +341,23 @@ inline LS_INLINE color_type sr_sample_trilinear(const SR_Texture& tex, float x, 
 
     namespace math = ls::math;
 
-    // use "-1" to avoid out-of-bounds errors at texture edges.
-    x = wrapMode(x) * ((float)tex.width()-1.f);
-    y = wrapMode(y) * ((float)tex.height()-1.f);
+    x = wrapMode(x) * (float)tex.width();
+    const uint16_t xi = (uint16_t)x;
+    const uint16_t si = (uint16_t)math::max(x-1.f, 0.f);
 
-    // only use fixed-point calculation for determining texel indices.
-    const fixed_type x0 = ls::math::fixed_cast<fixed_type, float>(x);
-    const fixed_type y0 = ls::math::fixed_cast<fixed_type, float>(y);
-    const uint16_t   xi = ls::math::integer_cast<uint16_t, fixed_type>(x0);
-    const uint16_t   yi = ls::math::integer_cast<uint16_t, fixed_type>(y0);
+    y = wrapMode(y) * (float)tex.height();
+    const uint16_t yi = (uint16_t)y;
+    const uint16_t ti = (uint16_t)math::max(y-1.f, 0.f);
 
-    constexpr fixed_type one = math::fixed_cast<fixed_type, int>(1);
-    constexpr fixed_type zero = math::fixed_cast<fixed_type, int>(0);
-    const uint16_t si = math::integer_cast<uint16_t, fixed_type>(math::max(x0-one, zero));
-    const uint16_t ti = math::integer_cast<uint16_t, fixed_type>(math::max(y0-one, zero));
-
-    const math::vec2_t<uint16_t> uv00 = {si, ti};
-    const math::vec2_t<uint16_t> uv10 = {xi, ti};
-    const math::vec2_t<uint16_t> uv01 = {si, yi};
-    const math::vec2_t<uint16_t> uv11 = {xi, yi};
-
-    const auto&& c000 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(uv00[0], uv00[1]));
-    const auto&& c100 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(uv10[0], uv10[1]));
-    const auto&& c010 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(uv01[0], uv01[1]));
-    const auto&& c110 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(uv11[0], uv11[1]));
+    const auto&& c000 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(si, ti));
+    const auto&& c100 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(xi, ti));
+    const auto&& c010 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(si, yi));
+    const auto&& c110 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(xi, yi));
 
     // floating-point math can be used for calculating the texel weights
     const float xf = x - math::floor(x);
-    const float xd = 1.f - xf;
     const float yf = y - math::floor(y);
+    const float xd = 1.f - xf;
     const float yd = 1.f - yf;
 
     const auto&& weight000 = c000 * xd*yd;
@@ -391,7 +378,6 @@ inline LS_INLINE color_type sr_sample_trilinear(const SR_Texture& tex, float x, 
         return color_type{0};
     }
 
-    typedef typename SR_Texture::fixed_type fixed_type;
     constexpr WrapMode wrapMode;
 
     /*
@@ -407,50 +393,33 @@ inline LS_INLINE color_type sr_sample_trilinear(const SR_Texture& tex, float x, 
 
     namespace math = ls::math;
 
-    // use "-1" to avoid out-of-bounds errors at texture edges.
-    x = wrapMode(x) * ((float)tex.width()  - 1.f);
-    y = wrapMode(y) * ((float)tex.height() - 1.f);
-    z = wrapMode(z) * ((float)tex.depth()  - 1.f);
+    x = wrapMode(x) * (float)tex.width();
+    const uint16_t xi = (uint16_t)x;
+    const uint16_t si = math::max(x-1.f, 0.f);
 
-    // only use fixed-point calculation for determining texel indices.
-    const fixed_type x0 = ls::math::fixed_cast<fixed_type, float>(x);
-    const fixed_type y0 = ls::math::fixed_cast<fixed_type, float>(y);
-    const fixed_type z0 = ls::math::fixed_cast<fixed_type, float>(z);
-    const uint16_t   xi = ls::math::integer_cast<uint16_t, fixed_type>(x0);
-    const uint16_t   yi = ls::math::integer_cast<uint16_t, fixed_type>(y0);
-    const uint16_t   zi = ls::math::integer_cast<uint16_t, fixed_type>(z0);
+    y = wrapMode(y) * (float)tex.height();
+    const uint16_t yi = (uint16_t)y;
+    const uint16_t ti = math::max(y-1.f, 0.f);
 
-    constexpr fixed_type one = math::fixed_cast<fixed_type, int>(1);
-    constexpr fixed_type zero = math::fixed_cast<fixed_type, int>(0);
-    const uint16_t si = math::integer_cast<uint16_t, fixed_type>(math::max(x0-one, zero));
-    const uint16_t ti = math::integer_cast<uint16_t, fixed_type>(math::max(y0-one, zero));
-    const uint16_t ri = math::integer_cast<uint16_t, fixed_type>(math::max(z0-one, zero));
+    z = wrapMode(z) * (float)tex.depth();
+    const uint16_t zi = (uint16_t)z;
+    const uint16_t ri = math::max(z-1.f, 0.f);
 
-    const uint16_t uv000[3] = {si, ti, ri};
-    const uint16_t uv100[3] = {xi, ti, ri};
-    const uint16_t uv010[3] = {si, yi, ri};
-    const uint16_t uv001[3] = {si, ti, zi};
-    const uint16_t uv101[3] = {xi, ti, zi};
-    const uint16_t uv011[3] = {si, yi, zi};
-    const uint16_t uv110[3] = {xi, yi, ri};
-    const uint16_t uv111[3] = {xi, yi, zi};
+    const auto&& c000 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(si, ti, ri));
+    const auto&& c100 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(xi, ti, ri));
+    const auto&& c010 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(si, yi, ri));
+    const auto&& c001 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(si, ti, zi));
+    const auto&& c101 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(xi, ti, zi));
+    const auto&& c011 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(si, yi, zi));
+    const auto&& c110 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(xi, yi, ri));
+    const auto&& c111 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(xi, yi, zi));
 
-    // floating-point math can be used for calculating the texel weights
     const float xf = x - math::floor(x);
     const float yf = y - math::floor(y);
     const float zf = z - math::floor(z);
     const float xd = 1.f - xf;
     const float yd = 1.f - yf;
     const float zd = 1.f - zf;
-
-    const auto&& c000 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(uv000[0], uv000[1], uv000[2]));
-    const auto&& c100 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(uv100[0], uv100[1], uv100[2]));
-    const auto&& c010 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(uv010[0], uv010[1], uv010[2]));
-    const auto&& c001 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(uv001[0], uv001[1], uv001[2]));
-    const auto&& c101 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(uv101[0], uv101[1], uv101[2]));
-    const auto&& c011 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(uv011[0], uv011[1], uv011[2]));
-    const auto&& c110 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(uv110[0], uv110[1], uv110[2]));
-    const auto&& c111 = color_cast<float, typename color_type::value_type>(tex.texel<color_type, order>(uv111[0], uv111[1], uv111[2]));
 
     const auto&& weight000 = c000 * xd*yd*zd;
     const auto&& weight100 = c100 * xf*yd*zd;
