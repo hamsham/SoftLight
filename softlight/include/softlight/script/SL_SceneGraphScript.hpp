f262f70808058dc4de4be7d6f43a58233191b599
hammy <mileslacey@gmail.com>
2021-10-17 17:27:58 -0700

    Using alternate fast inverse-sqrt function with less error. Fixed detection of x86-FMA during compilation.

---
+++ softlight/include/softlight/script/SL_SceneGraphScript.hpp
@@ -0,0 +1,25 @@
+//
+// Created by hammy on 2021-10-03.
+//
+
+#ifndef SL_SCENE_GRAPH_SCRIPT_HPP
+#define SL_SCENE_GRAPH_SCRIPT_HPP
+
+#include "lightsky/script/ScriptVariable.h"
+
+class SL_SceneGraph;
+
+
+
+namespace slscript
+{
+
+
+
+LS_SCRIPT_DECLARE_VAR(SL_SceneGraphScript, SL_SceneGraph*);
+
+
+
+} // end slscript namespace
+
+#endif /* SL_SCENE_GRAPH_SCRIPT_HPP */
---
+++ softlight/src/script/SL_SceneGraphScript.cpp
@@ -0,0 +1,11 @@
+//
+// Created by hammy on 2021-10-03.
+//
+
+#include "softlight/SL_SceneGraph.hpp"
+
+#include "softlight/script/SL_SceneGraphScript.hpp"
+
+
+
+LS_SCRIPT_DEFINE_VAR(SL_SceneGraphScript, SL_SceneGraph*);
