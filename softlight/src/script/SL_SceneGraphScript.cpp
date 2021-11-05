9ef6f15a09e038263f57945c059c8c35e0652ee6
hammy <mileslacey@gmail.com>
2021-11-05 12:51:37 -0700

    Preparing for addition of serialization support.

--- softlight/src/script/SL_SceneGraphScript.cpp
+++ softlight/src/script/SL_SceneGraphScript.cpp
@@ -2,10 +2,33 @@
 // Created by hammy on 2021-10-03.
 //
 
-#include "softlight/SL_SceneGraph.hpp"
+#include "lightsky/script/ScriptFactory.h"
 
+#include "softlight/SL_SceneGraph.hpp"
 #include "softlight/script/SL_SceneGraphScript.hpp"
 
 
 
 LS_SCRIPT_DEFINE_VAR(SL_SceneGraphScript, SL_SceneGraph*);
+
+/*-------------------------------------
+ * Scene Graph Serialization
+-------------------------------------*/
+LS_SCRIPT_OVERRIDE_VAR_SAVE(LS_STATIC_API, SL_SceneGraph*)
+{
+    const SL_SceneGraph* pGraph = data;
+    ostr << (const void*)pGraph;
+    return ostr.good();
+}
+
+/*-------------------------------------
+ * Scene Graph Deserialization
+-------------------------------------*/
+LS_SCRIPT_OVERRIDE_VAR_LOAD(LS_EXPORT_API, SL_SceneGraph*)
+{
+    (void)varImporter;
+    (void)funcImporter;
+    istr.read((char*)data, sizeof(SL_SceneGraph*));
+
+    return istr.good() || istr.eof();
+}
